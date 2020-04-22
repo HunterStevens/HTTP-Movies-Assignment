af73e2258c33437a6fe84058ee352cf8f001b222
Hunter Stevens <hunterstevens1996@gmail.com>
2020-04-22 13:36:40 -0600

    Filled out the form template for update Movie

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -2,10 +2,21 @@ import React, {useEffect} from 'react';
 import {useHistory} from 'react-router-dom';
 import axios from "axios";
 
-const UpdateMovie = props =>{
+export const UpdateMovie = props =>{
 
+    const makeUpdate= event =>{
+
+    }
 
     return(
-        
+        <div className="movie-card">
+            <form onSubmit={makeUpdate}>
+                <input type="text" placeholder="Title"/>
+                <input type="text" placeholder="Director"/>
+                <input type="text" placeholder="Metascore"/>
+                <input type="text" placeholder="Actors"/>
+                <button>Save Update</button>
+            </form>
+        </div>
     )
 }
