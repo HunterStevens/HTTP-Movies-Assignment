6b56cc94b38696f390459fc3144e964019604daf
Hunter Stevens <hunterstevens1996@gmail.com>
2020-04-22 13:19:57 -0600

    made new route for update movie in App and made UpdateMovie.js component

---
+++ client/src/Movies/UpdateMovie.js
@@ -0,0 +1,11 @@
+import React, {useEffect} from 'react';
+import {useHistory} from 'react-router-dom';
+import axios from "axios";
+
+const UpdateMovie = props =>{
+
+
+    return(
+        
+    )
+}
